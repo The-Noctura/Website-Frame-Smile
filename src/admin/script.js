6fcf2d728bfbe58463257395dce946b61bec9d54
The Noctura <muhamadalghani584@gmail.com>
2026-02-03 10:39:31 +0700

    update sidebar icon + collapsed function

--- src/admin/script.js
+++ src/admin/script.js
@@ -1,6 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const sidebar = document.querySelector('.side-bar');
 const toggle = document.querySelector('.sidebar-header');
 
-toggle.addEventListener('click', () => {
+  // 1. Ambil state dari localStorage saat halaman dibuka
+  const isCollapsed = localStorage.getItem('sidebarCollapsed') === 'true';
+  if (isCollapsed) {
+    sidebar.classList.add('collapsed');
+  }
+
+  // 2. Toggle sidebar + simpan state
+  toggle.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
+
+    localStorage.setItem(
+      'sidebarCollapsed',
+      sidebar.classList.contains('collapsed')
+    );
+  });
 });
